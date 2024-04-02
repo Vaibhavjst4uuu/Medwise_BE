d64bd89b6f2821a669b127b17278e4ed989dbcbb
Vaibhav Raj <vaibhavjst4uu@gmail.com>
2024-04-02 14:56:04 +0530

    removed host

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
-const app_host = process.env.APP_HOST;
+const port = process.env.PORT || 8000;
+// const app_host = process.env.APP_HOST;
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const path = require("path");
@@ -55,6 +55,6 @@ app.get("/a", (req,res)=>{
 
 app.use("/", indexRouter);
 
-app.listen(port,app_host, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
